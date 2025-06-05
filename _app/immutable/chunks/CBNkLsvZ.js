f2f9f5c5f40539aa7dee5f0c80f2f1c09eecafc2
Mannu Lambrichts <mannu346@gmail.com>
2025-06-05 16:10:00 +0100

    bugfix

---
+++ _app/immutable/chunks/CBNkLsvZ.js
@@ -0,0 +1,1 @@
+import{p}from"./CDEXxq_t.js";const o=p;export{o as p};
---
+++ _app/immutable/chunks/CDEXxq_t.js
@@ -0,0 +1,1 @@
+import{s as r,p as t}from"./DCHk910F.js";const a={get data(){return t.data},get error(){return t.error},get status(){return t.status}};r.updated.check;export{a as p};
